35b09fb1a94f939cc328e3aab10f9b078eaafcfc
trinhthinh <trinhthinh388@gmail.com>
2022-12-02 18:47:21 +0700

    update docker

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,4 +4,4 @@ echo "Chromium version: $(chromium-browser --version)"
 echo "Excutable path: $PUPPETEER_EXECUTABLE_PATH"
 echo "Starting Test Enviroment"
 
-yarn example serve & ./scripts/wait-for-it.sh localhost:3000 -- yarn renderer test --force-exit
+yarn example serve & ./scripts/wait-for-it.sh localhost:3000 -- yarn renderer test --verbose --forceExit
