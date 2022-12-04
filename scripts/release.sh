b3397927f4cf660a69b5b324c5598a1259b8b020
trinhthinh <trinhthinh388@gmail.com>
2022-12-04 22:14:10 +0700

    update

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+# Black        0;30     Dark Gray     1;30
+# Red          0;31     Light Red     1;31
+# Green        0;32     Light Green   1;32
+# Brown/Orange 0;33     Yellow        1;33
+# Blue         0;34     Light Blue    1;34
+# Purple       0;35     Light Purple  1;35
+# Cyan         0;36     Light Cyan    1;36
+# Light Gray   0;37     White         1;37
+
 set -euo pipefail
 
 SEMANTIC_VERSION=$1
 
-echo "Release with $SEMANTIC_VERSION version"
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
 
-echo "Packaging build files..."
+echo -e "${BLUE}Release with $SEMANTIC_VERSION version${NC}"
+
+echo "${BLUE}Packaging build files...${NC}"
 
 yarn renderer build
 
-yarn renderer version
+yarn npm whoami
