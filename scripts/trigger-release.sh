71c97d90f2a817a0557dd4ea9ead82e3cbc30c4d
trinhthinh <trinhthinh388@gmail.com>
2022-12-04 20:15:33 +0700

    update

--- scripts/trigger-release.sh
+++ scripts/trigger-release.sh
@@ -7,16 +7,16 @@ SEMANTIC_VERSION="$(buildkite-agent meta-data get "semantic-version")"
 # Create a pipeline with provided semantic version
 PIPELINE="steps:
   - command: make release
-  depends_on: prepare-puppeteer-release
-  plugins:
-    - ecr#v2.7.0:
-        login: true
-        account_ids: '014222686667'
-        region: 'ap-southeast-1'
-    - docker-compose#v3.9.0:
-        run: plugin
-        cache-from: plugin:014222686667.dkr.ecr.ap-southeast-1.amazonaws.com/opensource:latest
-        image-repository: 014222686667.dkr.ecr.ap-southeast-1.amazonaws.com/opensource
+    depends_on: prepare-puppeteer-release
+    plugins:
+      - ecr#v2.7.0:
+          login: true
+          account_ids: '014222686667'
+          region: 'ap-southeast-1'
+      - docker-compose#v3.9.0:
+          run: plugin
+          cache-from: plugin:014222686667.dkr.ecr.ap-southeast-1.amazonaws.com/opensource:latest
+          image-repository: 014222686667.dkr.ecr.ap-southeast-1.amazonaws.com/opensource
 "
 
 # Upload the new pipeline and add it to the current build
